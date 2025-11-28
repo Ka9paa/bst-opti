c3db4c59aaa2d3436a0091d325f2e141cb9a5bee
Ka9paa <caydenperson0@gmail.com>
2025-11-28 13:31:14 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
+```typescript
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../components/App';
+import App from '../App.tsx';
 import '../styles/globals.css';
-import { Toaster } from 'sonner';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Toaster position="top-right" richColors />
   </React.StrictMode>
 );
+```
+
